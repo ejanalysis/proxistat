b3583b9abee6f7f7b0cdf44050e4de26dc59b267
ejanalysis <info@ejanalysis.com>
2021-08-16 23:57:30 -0400

    bufferapi() added to use EJSCREEN buffering tool API, and more testpoints functions

---
+++ R/speedsummary.R
@@ -0,0 +1,39 @@
+#' Print summary of speed in buffers per hour, given start and end times of batch
+#'
+#' @param start time when finished from Sys.time()
+#' @param end time when finished from Sys.time()
+#' @param n how many buffers were done? 
+#'
+#' @return Prints to console and returns rate of buffers per hour
+#' @export
+#'
+#' @examples 
+#' # pts <- proxistat::testpoints_block2010(10)
+#' # benchmark.start <- Sys.time()
+#' # outlist <- proxistat::bufferapi(pts$lon, lat=pts$lat, radius = 5)
+#' # benchmark.end <- Sys.time()
+#' # proxistat::speedsummary(benchmark.start, benchmark.end, NROW(pts))
+#'  # VISUALIZE variability in speed
+#'  # hist(as.numeric(unlist(lapply(outlist, FUN =function(x) x$timeSeconds)), na.rm = T),100, 
+#'  #   main = 'Histogram of seconds per buffer', xlab='Seconds elapsed for a buffer query of API')
+#' 
+speedsummary <- function(start, end, n) {
+  
+  benchmark.start <- start; benchmark.end <- end 
+  # REPORT THE SPEED 
+  # based on benchmark.start <- Sys.time(); print('buffering done here'); benchmark.end <- Sys.time()
+  total.benchmark <- difftime(benchmark.end, benchmark.start) # automatic units like minutes or seconds
+  total.seconds <- difftime(benchmark.end, benchmark.start, units = 'secs') # in seconds
+  perhour <- round((dim(rnd.data)[1] / as.numeric(total.seconds))*3600,0)
+  print(paste0(
+    format(round((dim(rnd.data)[1] / as.numeric(total.seconds))*3600,0), big.mark=',', scientific=FALSE), 
+    ' buffers per hour (',
+    format(dim(rnd.data)[1],big.mark = ',', scientific = FALSE),
+    ' lat/long pairs per ',
+    format(round(as.numeric(total.seconds),0), big.mark = ',', scientific = FALSE),
+    ' seconds)'
+  ))
+  print(round(total.benchmark,1))
+  cat('Note that results were unavailable for', sum(sapply(dta.list, FUN = is.null)),'out of the', length(dta.list), 'buffers.', '\n')
+  return(perhour)
+  }
---
+++ R/testpoints_block2010.R
@@ -0,0 +1,30 @@
+#' Get lat lon for random US census blocks (where avg person lives or other weighting)
+#'
+#'  Note this is not optimized for speed- it can take 30 seconds to pick just 1,000 points if pop wtd. 
+#'  May recode this to allow wts to be pop (using blocks.pop), or area (using blocks.area), or none (no weight).
+#' @param size number of unique test points needed (no duplicates will be returned)
+#' @param popwtd default is TRUE, and pop weighted block points represent where the avg US resident lives.
+#'   If FALSE, unweighted, so each block has equal chance of being selected. 
+#' @return data.frame with columns lon and lat, in decimal degrees
+#' @export
+#'
+#' @examples 
+#' # pts <- proxistat::testpoints_block2010(10)
+#' # benchmark.start <- Sys.time()
+#' # outlist <- proxistat::bufferapi(pts$lon, lat=pts$lat, radius = 5)
+#' # benchmark.end <- Sys.time()
+#' # proxistat::speedsummary(benchmark.start, benchmark.end, NROW(pts))
+#'  # VISUALIZE variability in speed
+#'  # hist(as.numeric(unlist(lapply(outlist, FUN =function(x) x$timeSeconds)), na.rm = T),100, 
+#'  #   main = 'Histogram of seconds per buffer', xlab='Seconds elapsed for a buffer query of API')
+#'  
+testpoints_block2010 <- function(size, popwtd = TRUE) {
+  if (popwtd) {
+    randomrows <- sample(1:length(UScensus2010blocks::blocks.pop), size = size, replace = FALSE, prob = UScensus2010blocks::blocks.pop)
+  } else {
+    randomrows <- sample(1:length(UScensus2010blocks::blocks.fips), size = size, replace = FALSE)
+  }
+  pts <- data.frame(lon = UScensus2010blocks::blocks.lon[randomrows], lat = UScensus2010blocks::blocks.lat[randomrows])
+  return(pts)
+  #testpoints_x(x=UScensus2010blocks::blocks.lon[ , c('lon', 'lat')], size = size, prob = ejscreen::bg20$pop)
+}
