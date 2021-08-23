a6a863e9c0548f0778005880e9bb59dac39b2f1a
ejanalyst <info@ejanalysis.com>
2021-08-22 23:51:58 -0400

    Updated countiesall to 2021 but still missing 1 county (valdez ak) fips?

--- R/countiesall.R
+++ R/countiesall.R
@@ -1,30 +1,48 @@
 #' @name countiesall
 #' @docType data
 #' @aliases counties
-#' @title Counties information from U.S. Census Bureau from 2015
+#' @title Counties information from U.S. Census Bureau from 2021 
 #' @description This data set provides names and FIPS codes for U.S. Counties and County equivalents.
+#'  \preformatted{
+#' # How the 2021 version of proxistat::countiesall was created:
+#' 
+#' download.file( 'https://www2.census.gov/geo/docs/maps-data/data/gazetteer/2021_Gazetteer/2021_Gaz_counties_national.zip' , 'countyinfo.zip')
+#' unzip('countyinfo.zip', '2021_Gaz_counties_national.txt')
+#' 
+#' x <- readr::read_tsv(
+#'   '~/Downloads/2021_Gaz_counties_national.txt',
+#'   # We do not need all of these columns, and want to rename the 3 we want:
+#'   # col_names = c('USPS',  'GEOID', 'ANSICODE', 'NAME', 'ALAND', 'AWATER', 'ALAND_SQMI', 'AWATER_SQMI', 'INTPTLAT', 'INTPTLONG'),
+#'   # col_types = 'ccccnnnnnn', skip = 1
+#'   col_names = c('ST',  'FIPS.COUNTY',  'countyname'),
+#'   col_types = 'cc-c------', skip = 1
+#' )
+#' x <- as.data.frame(x)
+#' x$statename <- proxistat::lookup.states[ match(x[ , 'ST'], proxistat::lookup.states[ , 'ST']), 'statename']
+#' x$fullname <- apply( x[ , c('countyname', 'statename')] , 1, function(myrow) paste(myrow[1], myrow[2], sep=', '))
+#' countiesall <- x
+#' save(countiesall, file = '~/Downloads/countiesall.rdata')
+#' }
+#' 
 #' @seealso \code{\link[ejanalysis]{get.county.info}} from the \pkg{ejanalysis} package (\url{http://ejanalysis.github.io/ejanalysis/}), 
 #'   and the \pkg{UScensus2010county} package and the \pkg{acs} package
 #'   and the Census 2010 packages \url{http://lakshmi.calit2.uci.edu/census2000/} and \url{http://www.jstatsoft.org/v37/i06}
-#' @source Derived from Census Bureau, obtained early 2015.
+#' @source Derived from Census Bureau, obtained 8/2021.
 #' @keywords datasets
-#' @format A data.frame of 3234 U.S. Counties or County equivalents, with 5 variables. \cr\cr
+#' @format A data.frame of 3221 U.S. Counties or County equivalents, with 5 variables. \cr\cr
 #' > str(countiesall) \cr
-#'  'data.frame':	3235 obs. of  5 variables:
+#'  'data.frame':	3221 obs. of  5 variables:
 #' \itemize{
 #' \item  $ ST         : chr  "AL" "AL" "AL" "AL" ...
-#' \item  $ countyname : chr  "Autauga County" "Baldwin County" "Barbour County" "Bibb County" ...
 #' \item  $ FIPS.COUNTY: chr  "01001" "01003" "01005" "01007" ...
+#' \item  $ countyname : chr  "Autauga County" "Baldwin County" "Barbour County" "Bibb County" ...
 #' \item  $ statename  : chr  "Alabama" "Alabama" "Alabama" "Alabama" ...
 #' \item  $ fullname   : chr  "Autauga County, Alabama" "Baldwin County, Alabama" "Barbour County, Alabama" "Bibb County, Alabama" ...
 #' }
 #' \code{> head(countiesall) \cr \cr
-#'    ST     countyname FIPS.COUNTY statename                fullname \cr
-#'  1 AL Autauga County       01001   Alabama Autauga County, Alabama \cr
-#'  2 AL Baldwin County       01003   Alabama Baldwin County, Alabama \cr
-#'  3 AL Barbour County       01005   Alabama Barbour County, Alabama \cr
-#'  4 AL    Bibb County       01007   Alabama    Bibb County, Alabama \cr
-#'  5 AL  Blount County       01009   Alabama  Blount County, Alabama \cr
-#'  6 AL Bullock County       01011   Alabama Bullock County, Alabama \cr
+#'   ST FIPS.COUNTY     countyname statename                fullname\cr
+#' 1 AL       01001 Autauga County   Alabama Autauga County, Alabama\cr
+#' 2 AL       01003 Baldwin County   Alabama Baldwin County, Alabama\cr
+#' 3 AL       01005 Barbour County   Alabama Barbour County, Alabama\cr
 #'   }
 NULL
