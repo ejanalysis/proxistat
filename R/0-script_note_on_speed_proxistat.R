99dcda3ba0e64d74e2c67cda50010163a5d24ba3
ejanalyst <info@ejanalysis.com>
2022-04-26 13:17:47 -0400

    testpoints creation is super slow n

---
+++ R/0-script_note_on_speed_proxistat.R
@@ -0,0 +1,12 @@
+
+# speed of proxistat::get.distances.all  
+# which uses sf package to get distance pairs
+if (1 == 0) {
+  tp = EJAM::points1000example
+  tp = tp[!is.na(tp$LONG),]
+  names(tp) <- c('lon', 'lat')
+  Sys.time(); x=get.distances.all(tp, tp, return.crosstab=TRUE); Sys.time()
+  
+  tp10k <- testpoints_block2010(10000) # super slow
+  tp10k <- tp10k[!is.na(tp10k$lon),]  
+}
