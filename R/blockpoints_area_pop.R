4109299773ea4dbbf57ed711657585e935f6a270
Mark Corrales <corrales.mark@epa.gov>
2023-01-28 23:39:19 -0500

    Create blockpoints_area_pop.R

---
+++ R/blockpoints_area_pop.R
@@ -0,0 +1,10 @@
+#' @name blockpoints_area_pop
+#' @docType data
+#' @title Block points from Census Bureau for 2020 geographies
+#' @description data.table columns are: blockfips, lat, lon, pop, area, blockid
+#' @details Area is in square meters. 
+#'   This data set provides a data.table with 8,132,968 blocks  
+#'   It has PR but lacks "AS" "GU" "MP" "VI" 
+#'   
+#' @source Derived from Census Bureau, obtained 10/2022. 
+NULL
